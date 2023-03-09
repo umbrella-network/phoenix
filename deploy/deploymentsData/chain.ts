28e6e7f02c74fdba936359d6153ea07e8e850051
DZariusz <dzariusz@gmail.com>
2023-03-09 20:07:08 +0100

    chore: set 60sec padding for arbitrum for beta testing

--- deploy/deploymentsData/chain.ts
+++ deploy/deploymentsData/chain.ts
@@ -63,7 +63,7 @@ export const chainDeploymentData = async (hre: HardhatRuntimeEnvironment): Promi
       break;
 
     case networks.ARBITRUM_PRODUCTION:
-      padding = 65535;
+      padding = 60;
       requiredSignatures = 6;
       allowForMixedType = false;
       break;
