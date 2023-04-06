c8afbcc031821097760bb2697daac0d5cba20ce9
DZariusz <dzariusz@gmail.com>
2023-04-06 17:46:00 +0200

    chore: - re-deploy Arbitrum with padding 6h

--- deploy/deploymentsData/chain.ts
+++ deploy/deploymentsData/chain.ts
@@ -63,7 +63,7 @@ export const chainDeploymentData = async (hre: HardhatRuntimeEnvironment): Promi
       break;
 
     case networks.ARBITRUM_PRODUCTION:
-      padding = 65535;
+      padding = 60 * 60 * 6; // 6h
       requiredSignatures = 6;
       allowForMixedType = false;
       break;
