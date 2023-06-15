f8cbb9f97a6365262e9149ce8cd6174baf74e786
DZariusz <dzariusz@gmail.com>
2023-06-15 10:43:54 +0200

    feat: bulk verification to save gas

--- foundry/test/SignerHelper.sol
+++ foundry/test/SignerHelper.sol
@@ -7,23 +7,39 @@ import "../lib/Mock.sol";
 
 import "../../contracts/onChainFeeds/UmbrellaFeeds.sol";
 import "../../contracts/StakingBankState.sol";
+import "../../contracts/stakingBankStatic/StakingBankStatic.sol";
+import "../../contracts/stakingBankStatic/StakingBankStaticCI.sol";
 
 
 contract SignerHelper is DSTest {
-    // 0x2ffd013aaa7b5a7da93336c2251075202b33fb2b
     uint256 constant PK1 = 0x87630b2d1de0fbd5044eb6891b3d9d98c34c8d310c852f98550ba774480e47cc;
-    // 0xe5904695748fe4a84b40b3fc79de2277660bd1d3
-    uint256 constant PK2 = 0x23c601ae397441f3ef6f1075dcb0031ff17fb079837beadaf3c84d96c6f3e569;
+    uint256 constant PK2 = 0x3f1e8b94c70206bf816c1ed0b15ad98bdf225ae4c6e7e4eee6cdbcf706fda2ae;
+    uint256 constant PK3 = 0x5da6b84117504d06b5dcd52b990d76965d2882f4e5852eb610bc76e4209b10d7;
+    uint256 constant PK4 = 0x1e5012671de3332ad0b43661984e94ab0e405bffddc9d3e863055040bab354b8;
+    uint256 constant PK5 = 0x0edc1e35ea7701ddac703286674e79f04addbf5d2f6162fabc19d39bd3dc6662;
+    uint256 constant PK6 = 0x23c601ae397441f3ef6f1075dcb0031ff17fb079837beadaf3c84d96c6f3e569;
+    uint256 constant PK_INVALID = uint256(0x11); // 0x252Dae0A4b9d9b80F504F6418acd2d364C0c59cD
 
-    address public immutable bank;
+    IStakingBank public immutable bank;
 
     CheatCodes constant cheats = CheatCodes(HEVM_ADDRESS);
 
     constructor() {
-        bank = Mock.create("StakingBank");
+        bank = new StakingBankStaticCI(6);
+    }
+
+    function _pk(uint256 _i) internal pure returns (uint256) {
+        if (_i == 0) return PK1;
+        if (_i == 1) return PK2;
+        if (_i == 2) return PK3;
+        if (_i == 3) return PK4;
+        if (_i == 4) return PK5;
+        if (_i == 5) return PK6;
+        return PK_INVALID;
     }
 
     function _signData(
+        uint256 _numSigs,
         UmbrellaFeeds _feeds,
         bytes32[] memory _priceKeys,
         IUmbrellaFeeds.PriceData[] memory _priceDatas
@@ -31,26 +47,42 @@ contract SignerHelper is DSTest {
         bytes32 priceDataHash = keccak256(abi.encode(_feeds.getChainId(), address(_feeds), _priceKeys, _priceDatas));
         bytes32 hash = keccak256(abi.encodePacked(_feeds.ETH_PREFIX(), priceDataHash));
 
-        signatures = new UmbrellaFeeds.Signature[](2);
-        (signatures[0].v, signatures[0].r, signatures[0].s) = cheats.sign(PK1, hash);
-        (signatures[1].v, signatures[1].r, signatures[1].s) = cheats.sign(PK2, hash);
+        signatures = new UmbrellaFeeds.Signature[](_numSigs);
 
-        cheats.mockCall(bank, abi.encodeCall(StakingBankState.balanceOf, (cheats.addr(PK1))), abi.encode(uint256(1e18)));
-        cheats.mockCall(bank, abi.encodeCall(StakingBankState.balanceOf, (cheats.addr(PK2))), abi.encode(uint256(1e18)));
+        for (uint256 i; i < _numSigs; i++) {
+            (signatures[i].v, signatures[i].r, signatures[i].s) = cheats.sign(_pk(i), hash);
+        }
+    }
+
+    function _invalidSignData(
+        uint256 _numSigs,
+        UmbrellaFeeds _feeds,
+        bytes32[] memory _priceKeys,
+        IUmbrellaFeeds.PriceData[] memory _priceDatas
+    ) internal returns (IUmbrellaFeeds.Signature[] memory signatures) {
+        bytes32 priceDataHash = keccak256(abi.encode(_feeds.getChainId(), address(_feeds), _priceKeys, _priceDatas));
+        bytes32 hash = keccak256(abi.encodePacked(_feeds.ETH_PREFIX(), priceDataHash));
+
+        signatures = new UmbrellaFeeds.Signature[](_numSigs);
+
+        for (uint256 i; i < _numSigs; i++) {
+            (signatures[i].v, signatures[i].r, signatures[i].s) =
+                i == 0 ? cheats.sign(_pk(999), hash) : cheats.sign(_pk(i), hash);
+        }
     }
 
     function _signReset(
+        uint256 _numSigs,
         UmbrellaFeeds _feeds,
         bytes32[] memory _priceKeys
     ) internal returns (IUmbrellaFeeds.Signature[] memory signatures) {
         bytes32 resetHash = keccak256(abi.encodePacked(_feeds.getChainId(), address(_feeds), _priceKeys, "RESET"));
         bytes32 hash = keccak256(abi.encodePacked(_feeds.ETH_PREFIX(), resetHash));
 
-        signatures = new UmbrellaFeeds.Signature[](2);
-        (signatures[0].v, signatures[0].r, signatures[0].s) = cheats.sign(PK1, hash);
-        (signatures[1].v, signatures[1].r, signatures[1].s) = cheats.sign(PK2, hash);
+        signatures = new UmbrellaFeeds.Signature[](_numSigs);
 
-        cheats.mockCall(bank, abi.encodeCall(StakingBankState.balanceOf, (cheats.addr(PK1))), abi.encode(uint256(1e18)));
-        cheats.mockCall(bank, abi.encodeCall(StakingBankState.balanceOf, (cheats.addr(PK2))), abi.encode(uint256(1e18)));
+        for (uint256 i; i < _numSigs; i++) {
+            (signatures[i].v, signatures[i].r, signatures[i].s) = cheats.sign(_pk(i), hash);
+        }
     }
 }
--- foundry/test/UmbrellaFeeds.t.sol
+++ foundry/test/UmbrellaFeeds.t.sol
@@ -10,7 +10,8 @@ import "./SignerHelper.sol";
 contract UmbrellaFeedsTest is SignerHelper {
     address public immutable registry;
     UmbrellaFeeds public immutable feeds;
-    UmbrellaFeeds public immutable feeds2;
+    UmbrellaFeeds public immutable feeds1;
+    UmbrellaFeeds public immutable feeds6;
 
     bytes32[] priceKeys;
     UmbrellaFeeds.PriceData[] priceDatas;
@@ -20,7 +21,8 @@ contract UmbrellaFeedsTest is SignerHelper {
 
         cheats.mockCall(registry, abi.encodeCall(Registry.requireAndGetAddress, ("StakingBank")), abi.encode(bank));
         feeds = new UmbrellaFeeds(IRegistry(registry), 2, 8);
-        feeds2 = new UmbrellaFeeds(IRegistry(registry), 1, 8);
+        feeds1 = new UmbrellaFeeds(IRegistry(registry), 1, 8);
+        feeds6 = new UmbrellaFeeds(IRegistry(registry), 6, 8);
 
         priceKeys.push(keccak256(abi.encodePacked("UMB-USD")));
         priceKeys.push(keccak256(abi.encodePacked("ARB-USD")));
@@ -30,7 +32,6 @@ contract UmbrellaFeedsTest is SignerHelper {
     }
 
     function test_UmbrellaFeeds_getPrice_failWhenNoPriceAndNoFallback() public {
-        cheats.mockCall(bank, abi.encodeCall(StakingBankState.balanceOf, (0xDc3eBc37DA53A644D67E5E3b5BA4EEF88D969d5C)), abi.encode(uint256(1e18)));
         cheats.mockCall(registry, abi.encodeCall(Registry.getAddressByString, (feeds.NAME())), abi.encode(address(feeds)));
 
         cheats.expectRevert(UmbrellaFeeds.FeedNotExist.selector);
@@ -38,8 +39,7 @@ contract UmbrellaFeedsTest is SignerHelper {
     }
 
     function test_UmbrellaFeeds_getPrice_failWhenNoPriceWithFallback() public {
-        cheats.mockCall(bank, abi.encodeCall(StakingBankState.balanceOf, (0xDc3eBc37DA53A644D67E5E3b5BA4EEF88D969d5C)), abi.encode(uint256(1e18)));
-        cheats.mockCall(registry, abi.encodeCall(Registry.getAddressByString, (feeds.NAME())), abi.encode(address(feeds2)));
+        cheats.mockCall(registry, abi.encodeCall(Registry.getAddressByString, (feeds.NAME())), abi.encode(address(feeds1)));
 
         cheats.expectRevert(UmbrellaFeeds.FeedNotExist.selector);
         feeds.getPriceData(bytes32(0));
@@ -51,30 +51,62 @@ contract UmbrellaFeedsTest is SignerHelper {
     function test_UmbrellaFeeds_debug() public {
         bytes32[] memory keys = new bytes32[](1);
         IUmbrellaFeeds.PriceData[] memory datas = new IUmbrellaFeeds.PriceData[](1);
+        IUmbrellaFeeds.PriceData[] memory datas2 = new IUmbrellaFeeds.PriceData[](1);
 
         keys[0] = keccak256(abi.encodePacked("UMB-USD"));
         datas[0] = IUmbrellaFeeds.PriceData(0,86400,1683651196,1195497);
 
-        IUmbrellaFeeds.Signature[] memory sigs = _signData(feeds, keys, datas);
-        IUmbrellaFeeds.Signature[] memory sigs2 = _signData(feeds2, keys, datas);
+        IUmbrellaFeeds.Signature[] memory sigs = _signData(2, feeds, keys, datas);
+        IUmbrellaFeeds.Signature[] memory sigs1 = _signData(1, feeds1, keys, datas);
+        IUmbrellaFeeds.Signature[] memory sigs6 = _signData(6, feeds6, keys, datas);
 
         uint256 gasStart = gasleft();
         feeds.update(keys, datas, sigs);
         uint256 gasUsed = gasStart - gasleft();
 
-        emit log_named_uint("gas used to update with 2 signatures:", gasUsed);
+        emit log_named_uint("gas used to update with 2 signatures (initial):", gasUsed);
+        assertEq(gasUsed, 41354);
+
+        datas2[0] = IUmbrellaFeeds.PriceData(0, datas[0].heartbeat, datas[0].timestamp + 2, datas[0].price + 10e8);
+        sigs = _signData(2, feeds, keys, datas2);
 
         gasStart = gasleft();
-        feeds2.update(keys, datas, sigs2);
+        feeds.update(keys, datas2, sigs);
         gasUsed = gasStart - gasleft();
 
-        emit log_named_uint("gas used to update with 1 signature:", gasUsed);
+        emit log_named_uint("gas used to update with 2 signatures (#2):", gasUsed);
+        assertEq(gasUsed, 16954);
+
+        gasStart = gasleft();
+        feeds1.update(keys, datas, sigs1);
+        gasUsed = gasStart - gasleft();
+
+        emit log_named_uint("gas used to update with 1 signature (initial):", gasUsed);
+        assertEq(gasUsed, 33742);
+
+        gasStart = gasleft();
+        feeds6.update(keys, datas, sigs6);
+        gasUsed = gasStart - gasleft();
+
+        emit log_named_uint("gas used to update with 6 signature (initial):", gasUsed);
+        assertEq(gasUsed, 59331);
+
+        sigs6 = _signData(6, feeds6, keys, datas2);
+
+        gasStart = gasleft();
+        feeds6.update(keys, datas2, sigs6);
+        gasUsed = gasStart - gasleft();
+
+        emit log_named_uint("gas used to update with 6 signature (#2):", gasUsed);
+        assertEq(gasUsed, 37502);
 
         gasStart = gasleft();
         IUmbrellaFeeds.PriceData memory result = feeds.getPriceData(keys[0]);
         gasUsed = gasStart - gasleft();
 
         emit log_named_uint("gas used to read price directly:", gasUsed);
+        emit log_named_uint("result:", result.price);
+        assertEq(gasUsed, 1951);
     }
 
     /*
@@ -95,18 +127,18 @@ contract UmbrellaFeedsTest is SignerHelper {
     forge test -vvv --match-test test_UmbrellaFeeds_getPrice_withFallback
     */
     function test_UmbrellaFeeds_getPrice_withFallback() public {
-        _executeUpdate(feeds2);
+        _executeUpdate(feeds1);
 
         UmbrellaFeeds.PriceData memory expectedData = priceDatas[0];
 
-        cheats.mockCall(registry, abi.encodeCall(Registry.getAddressByString, (feeds.NAME())), abi.encode(address(feeds2)));
+        cheats.mockCall(registry, abi.encodeCall(Registry.getAddressByString, (feeds.NAME())), abi.encode(address(feeds1)));
         UmbrellaFeeds.PriceData memory data = feeds.getPriceData(priceKeys[0]);
 
         assertEq(data.price, expectedData.price);
     }
 
     function test_UmbrellaFeeds_update_ignoringOtherSignatures() public {
-        UmbrellaFeeds.Signature[] memory signatures = _signData(feeds, priceKeys, priceDatas);
+        UmbrellaFeeds.Signature[] memory signatures = _signData(2, feeds, priceKeys, priceDatas);
         UmbrellaFeeds.Signature[] memory moreSignatures = new UmbrellaFeeds.Signature[](3);
         (moreSignatures[0], moreSignatures[1]) = (signatures[0], signatures[1]);
         // third one is empty, but ignored
@@ -115,19 +147,18 @@ contract UmbrellaFeedsTest is SignerHelper {
     }
 
     function test_UmbrellaFeeds_update_failWhenWantToReuseSignatureInOtherContract() public {
-        UmbrellaFeeds.Signature[] memory signatures = _signData(feeds, priceKeys, priceDatas);
+        UmbrellaFeeds.Signature[] memory signatures = _signData(2, feeds, priceKeys, priceDatas);
 
         // pass
         feeds.update(priceKeys, priceDatas, signatures);
 
-        cheats.mockCall(bank, abi.encodeCall(StakingBankState.balanceOf, (0x341f952eE98298579117344E1c506a2D261DC2dc)), abi.encode(uint256(0)));
         cheats.expectRevert(UmbrellaFeeds.InvalidSigner.selector);
         // fail
-        feeds2.update(priceKeys, priceDatas, signatures);
+        feeds1.update(priceKeys, priceDatas, signatures);
     }
 
     function test_UmbrellaFeeds_update_failWhenSignatureUsedAgain() public {
-        UmbrellaFeeds.Signature[] memory signatures = _signData(feeds, priceKeys, priceDatas);
+        UmbrellaFeeds.Signature[] memory signatures = _signData(2, feeds, priceKeys, priceDatas);
 
         // pass
         feeds.update(priceKeys, priceDatas, signatures);
@@ -137,24 +168,24 @@ contract UmbrellaFeedsTest is SignerHelper {
     }
 
     function test_UmbrellaFeeds_update_failWhenNotEnoughSignatures() public {
-        UmbrellaFeeds.Signature[] memory signatures = _signData(feeds, priceKeys, priceDatas);
         UmbrellaFeeds.Signature[] memory oneSignature = new UmbrellaFeeds.Signature[](1);
 
         cheats.expectRevert(UmbrellaFeeds.NotEnoughSignatures.selector);
         feeds.update(priceKeys, priceDatas, oneSignature);
     }
 
+    /*
+    forge test -vvv --match-test test_UmbrellaFeeds_update_failWhenInvalidSigner
+    */
     function test_UmbrellaFeeds_update_failWhenInvalidSigner() public {
-        UmbrellaFeeds.Signature[] memory signatures = _signData(feeds, priceKeys, priceDatas);
-
-        cheats.mockCall(bank, abi.encodeCall(StakingBankState.balanceOf, (cheats.addr(PK1))), abi.encode(uint256(0)));
+        UmbrellaFeeds.Signature[] memory signatures = _invalidSignData(2, feeds, priceKeys, priceDatas);
         cheats.expectRevert(UmbrellaFeeds.InvalidSigner.selector);
 
         feeds.update(priceKeys, priceDatas, signatures);
     }
 
     function test_UmbrellaFeeds_update_failWhenSignaturesNotInOrder() public {
-        UmbrellaFeeds.Signature[] memory signaturesNotOrdered = _signData(feeds, priceKeys, priceDatas);
+        UmbrellaFeeds.Signature[] memory signaturesNotOrdered = _signData(2, feeds, priceKeys, priceDatas);
         (signaturesNotOrdered[1], signaturesNotOrdered[0]) = (signaturesNotOrdered[0], signaturesNotOrdered[1]);
 
         cheats.expectRevert(UmbrellaFeeds.SignaturesOutOfOrder.selector);
@@ -162,7 +193,7 @@ contract UmbrellaFeedsTest is SignerHelper {
     }
 
     function test_UmbrellaFeeds_update_failNotEnoughSignatures() public {
-        UmbrellaFeeds.Signature[] memory signatures = _signData(feeds, priceKeys, priceDatas);
+        UmbrellaFeeds.Signature[] memory signatures = _signData(2, feeds, priceKeys, priceDatas);
         UmbrellaFeeds.Signature[] memory onesignature = new UmbrellaFeeds.Signature[](1);
         onesignature[0] = signatures[0];
 
@@ -174,7 +205,7 @@ contract UmbrellaFeedsTest is SignerHelper {
     forge test -vvv --match-test test_UmbrellaFeeds_reset
     */
     function test_UmbrellaFeeds_reset() public {
-        feeds.update(priceKeys, priceDatas, _signData(feeds, priceKeys, priceDatas));
+        feeds.update(priceKeys, priceDatas, _signData(2, feeds, priceKeys, priceDatas));
 
         assert(feeds.prices(priceKeys[0]).price != 0);
         assert(feeds.prices(priceKeys[1]).price != 0);
@@ -182,7 +213,7 @@ contract UmbrellaFeedsTest is SignerHelper {
         bytes32[] memory toRemove = new bytes32[](1);
         toRemove[0] = priceKeys[1];
 
-        UmbrellaFeeds.Signature[] memory signatures = _signReset(feeds, toRemove);
+        UmbrellaFeeds.Signature[] memory signatures = _signReset(2, feeds, toRemove);
 
         feeds.reset(toRemove, signatures);
 
@@ -191,10 +222,10 @@ contract UmbrellaFeedsTest is SignerHelper {
     }
 
     function test_UmbrellaFeeds_failWhenWantToUpdateResetedData() public {
-        UmbrellaFeeds.Signature[] memory oldSignatures = _signData(feeds, priceKeys, priceDatas);
+        UmbrellaFeeds.Signature[] memory oldSignatures = _signData(2, feeds, priceKeys, priceDatas);
         feeds.update(priceKeys, priceDatas, oldSignatures);
 
-        UmbrellaFeeds.Signature[] memory signatures = _signReset(feeds, priceKeys);
+        UmbrellaFeeds.Signature[] memory signatures = _signReset(2, feeds, priceKeys);
 
         feeds.reset(priceKeys, signatures);
 
@@ -203,7 +234,7 @@ contract UmbrellaFeedsTest is SignerHelper {
     }
 
     function _executeUpdate(UmbrellaFeeds _feeds) internal {
-        UmbrellaFeeds.Signature[] memory signatures = _signData(_feeds, priceKeys, priceDatas);
+        UmbrellaFeeds.Signature[] memory signatures = _signData(2, _feeds, priceKeys, priceDatas);
         _feeds.update(priceKeys, priceDatas, signatures);
     }
 }
