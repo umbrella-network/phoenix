c3a04aeb0afb6532f71ed080b0c6d28186847a75
DZariusz <dzariusz@gmail.com>
2024-04-29 11:16:18 +0200

    ...

--- contracts/fetchers/uniswapV3/UniswapV3QuoterV2.sol
+++ contracts/fetchers/uniswapV3/UniswapV3QuoterV2.sol
@@ -2,8 +2,5 @@
 pragma solidity =0.7.6;
 pragma abicoder v2;
 
+// we import this because we need artifacts
 import {QuoterV2} from "gitmodules/uniswap/v3-periphery/contracts/lens/QuoterV2.sol";
-
-//contract UniswapV3QuoterV2 is QuoterV2 {
-//    constructor(address _factory, address _WETH9) QuoterV2(_factory, _WETH9) {}
-//}
