93f705f1b1f2b12fc76bbbd394a409f4e7484de1
DZariusz <dzariusz@gmail.com>
2024-05-22 21:11:59 +0200

    import contracts task

---
+++ tasks/importContract.ts
@@ -0,0 +1,23 @@
+import { task } from 'hardhat/config';
+import { importContracToRegistry } from './_helpers/importContracToRegistry';
+
+/*
+npx hardhat importContract --network rootstock_sandbox --contract SovrynFetcherHelper
+ */
+task('importContract')
+  .addParam('contract', 'contract name')
+  .addFlag('update', 'if contract is already registered, use this flag to update')
+  .setAction(async (taskArgs, hre) => {
+    console.log('importing', taskArgs.contract);
+
+    const result = await importContracToRegistry(hre, taskArgs.contract, taskArgs.update);
+
+    if (!result) return;
+    const [tx, address] = result;
+
+    console.log(`importContracts tx #${tx.nonce} ${tx.hash}`);
+    console.log('waiting for confirmations...');
+    await tx.wait(1);
+
+    console.log(`${taskArgs.contract} (${address}) registered`);
+  });
